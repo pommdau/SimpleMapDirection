b6c932337ba7343577d4290e6022fd615e106fee
IKEH <pondau1024@gmail.com>
2019-07-14 12:05:33 +0900

    initial commit

---
+++ SimpleMapDirection/SimpleMapDirection/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SimpleMapDirection
+//
+//  Created by Hiroki Ikeuchi on 2019/07/14.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleMapDirection/SimpleMapDirection/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SimpleMapDirection
+//
+//  Created by Hiroki Ikeuchi on 2019/07/14.
+//  Copyright © 2019 ikeh1024. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
